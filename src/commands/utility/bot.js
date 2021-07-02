7d9f8aee328f5314d1206d379c51f164cdf295a3
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2021-07-02 21:13:49 +0700

    Base bot

---
+++ src/commands/utility/bot.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "bot",
+    execute(message) {
+        message.channel.send(
+            "I'm a mindless stupid bot with ``stupid `` prefix."
+        )
+    },
+}
---
+++ src/commands/utility/ping.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "ping",
+    description: "Information about the arguments provided.",
+    execute(message, args) {
+        message.channel.send("Im working bro")
+    },
+}
