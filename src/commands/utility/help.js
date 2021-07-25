c6f967a0bf5af999dd16d1f8f2cb41f780ad799c
betich <betich365@gmail.com>
2021-07-25 14:14:45 +0700

    :art: decorate help command

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -1,4 +1,5 @@
 const Embed = require('../../lib/Embed')
+const { prefix } = require('../../../config.json')
 
 module.exports = {
   name: 'help',
@@ -11,32 +12,43 @@ module.exports = {
       },
     },
   },
+  usage: "help [<'all'> [command]]",
   execute({ send, args, message }) {
     const { commands } = message.client;
 
     if (args.length > 0) {
-      const commandFields = commands
-        .filter((command) => args.includes(command.name))
-        .map((command) => {
-          return {
-            "name": `\`${command.name}\``,
-            "value": `${command.description}`
-          }
-        })
+      const command = commands
+        .find((command) => args[0] === command.name)
 
-      send(Embed.Embed(
-        "Help", "", "#7f03fc", [commandFields]
-      ))
-    } else {
-      const commandFields = commands.map((command) => {
+      if (!command) {
+        return send(Embed.SendError("Command not found", `type \`${prefix}help\` or \`/help\` to get a list of all commands`))
+      }
+
+      const commandFields = Object.entries(command)
+      .filter((field) => ['name', 'description', 'aliases', 'usage'].includes(field[0]))
+      .map((field) => {
+        return field[0] ===  'aliases' ? [field[0], field[1].join(", ")] : field
+      })
+      .map((field) => {
         return {
-          "name": `\`${command.name}\``,
-          "value": `${command.description}`
+          name: `\`${field[0]}\``,
+          value: `${field[1]}`
         }
       })
 
       send(Embed.Embed(
-        "Help", "", "#7f03fc", [commandFields]
+        `Help: ${command.name}`, `type \`${prefix}help\` or \`/help\` to get a list of all commands`, "#7f03fc", commandFields
+      ))
+    } else {
+      const commandFields = commands.map((command) => {
+        return {
+          name: `\`${command.name}\``,
+          value: `${command.description}`
+        }
+      })
+
+      send(Embed.Embed(
+        "Help", `type \`${prefix}help <command>\` or \`/help <command>\` to get more info about a command`, "#7f03fc", commandFields
       ))
     }
   },
