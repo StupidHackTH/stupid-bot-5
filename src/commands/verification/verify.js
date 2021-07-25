74728b56be716bb6be8e0560795efa3bdaafbd50
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2021-07-25 21:51:33 +0700

    change config

--- src/commands/verification/verify.js
+++ src/commands/verification/verify.js
@@ -1,23 +1,25 @@
-const { Embed } = require("../../lib/Embed")
+const Embed = require('../../lib/Embed')
 
 module.exports = {
-  name: 'verify',
-  description: 'verify your account with Eventpop reference code',
-  slash: {
-    registerData: {
-      data: {
-        name: 'verify',
-        description: 'verify your account with Eventpop reference code',
-      },
-    },
-  },
-  async execute({ send, member }) {
-    send(`Sent instructions in your DM, ${member}`)
-    member.createDM().then((channel) => {
-      channel.send(Embed.Embed(
-        'Verify',
-        'enter Eventpop reference code ``#XXXXX-XXXXXXX`` in THIS channel',
-      ))
-    })
-  },
+	name: 'verify',
+	description: 'verify your account with Eventpop reference code',
+	slash: {
+		registerData: {
+			data: {
+				name: 'verify',
+				description: 'verify your account with Eventpop reference code',
+			},
+		},
+	},
+	async execute({ send, member }) {
+		send(`Sent instructions in your DM, ${member}`)
+		member.createDM().then((channel) => {
+			channel.send(
+				Embed.Embed(
+					'Verify',
+					'enter Eventpop reference code ``#XXXXX-XXXXXXX`` in THIS channel',
+				),
+			)
+		})
+	},
 }
--- src/lib/Authentication.js
+++ src/lib/Authentication.js
@@ -5,101 +5,104 @@ const Embed = require('./Embed')
 module.exports = Authenticate
 
 async function Authenticate(client, refCode, message) {
-  if (!client.database) {
-    return message.channel.send('Database unavaiable')
-  }
+	if (!client.database) {
+		return message.channel.send('Database unavaiable')
+	}
 
-  const docId = 'g86J49lbnaDIYxEnn0Om'
+	const docId = 'g86J49lbnaDIYxEnn0Om'
 
-  const attendeeCollection = client.database
-    .collection('refcode')
-    .doc(docId)
-    .collection('attendees')
-  const participantCollection = client.database
-    .collection('refcode')
-    .doc(docId)
-    .collection('participants')
+	const attendeeCollection = client.database
+		.collection('refcode')
+		.doc(docId)
+		.collection('attendees')
+	const participantCollection = client.database
+		.collection('refcode')
+		.doc(docId)
+		.collection('participants')
 
-  const attendeeDocRef = attendeeCollection.doc(refCode)
-  const attendeeDocSnapshot = await attendeeDocRef.get()
-  const participantDocRef = participantCollection.doc(refCode)
-  const participantSnapshot = await participantDocRef.get()
+	const attendeeDocRef = attendeeCollection.doc(refCode)
+	const attendeeDocSnapshot = await attendeeDocRef.get()
+	const participantDocRef = participantCollection.doc(refCode)
+	const participantSnapshot = await participantDocRef.get()
 
-  if (attendeeDocSnapshot.exists) {
-    const { used } = attendeeDocSnapshot.data()
+	if (attendeeDocSnapshot.exists) {
+		const { used } = attendeeDocSnapshot.data()
 
-    if (used) {
-      return message.channel.send(
-        Embed.SendError(
-          'Error',
-          'This code is already used. Please contact staff',
-        ),
-      )
-    }
+		if (used) {
+			return message.channel.send(
+				Embed.SendError(
+					'Error',
+					'This code is already used. Please contact staff',
+				),
+			)
+		}
 
-    try {
-      await attendeeDocRef.set({
-        used: true,
-        used_by: message.author.id,
-        used_at: firebase.firestore.FieldValue.serverTimestamp(),
-      })
+		try {
+			await attendeeDocRef.set({
+				used: true,
+				used_by: message.author.id,
+				used_at: firebase.firestore.FieldValue.serverTimestamp(),
+			})
 
-      const guild = await client.guilds.fetch(guildId)
-      const participantRole = guild.roles.cache.find(
-        (e) => e.name === 'Attendee',
-      )
-      const guildMember = guild.members.cache.find(
-        (e) => e.id === message.author.id,
-      )
+			const guild = await client.guilds.fetch(guildId)
+			const attendeeRole = guild.roles.cache.find((e) => e.name === 'Attendee')
 
-      await guildMember.roles.add(participantRole)
+			const guildMember = guild.members.cache.find(
+				(e) => e.id === message.author.id,
+			)
 
-      await message.channel.send(
-        Embed.SendSuccess('Attendee Role Granted', 'Enjoy Hackathon!'),
-      )
-    } catch (e) {
-      console.error(e)
-      message.channel.send(Embed.SendError('Error', 'Please contact staff'))
-    }
-  }
+			await guildMember.roles.add(attendeeRole)
 
-  if (participantSnapshot.exists) {
-    const { used } = participantSnapshot.data()
+			await message.channel.send(
+				Embed.SendSuccess('Attendee Role Granted', 'Enjoy Hackathon!'),
+			)
+		} catch (e) {
+			console.error(e)
+			message.channel.send(Embed.SendError('Error', 'Please contact staff'))
+		}
+	}
 
-    if (used) {
-      return message.channel.send(
-        Embed.SendError(
-          'Error',
-          'This code is already used. Please contact staff',
-        ),
-      )
-    }
+	if (participantSnapshot.exists) {
+		const { used } = participantSnapshot.data()
 
-    try {
-      await docRef.set({
-        used: true,
-        used_by: message.author.id,
-        used_at: firebase.firestore.FieldValue.serverTimestamp(),
-      })
+		if (used) {
+			return message.channel.send(
+				Embed.SendError(
+					'Error',
+					'This code is already used. Please contact staff',
+				),
+			)
+		}
 
-      const guild = await client.guilds.fetch(guildId)
-      const participantRole = guild.roles.cache.find(
-        (e) => e.name === 'Participant',
-      )
-      const guildMember = guild.members.cache.find(
-        (e) => e.id === message.author.id,
-      )
+		try {
+			await docRef.set({
+				used: true,
+				used_by: message.author.id,
+				used_at: firebase.firestore.FieldValue.serverTimestamp(),
+			})
 
-      await guildMember.roles.add(participantRole)
+			const guild = await client.guilds.fetch(guildId)
+			const participantRole = guild.roles.cache.find(
+				(e) => e.name === 'Participant',
+			)
+			const guildMember = guild.members.cache.find(
+				(e) => e.id === message.author.id,
+			)
 
-      await message.channel.send(
-        Embed.SendSuccess('Participant Role Granted', 'Enjoy Hackathon!'),
-      )
-    } catch (e) {
-      console.error(e)
-      message.channel.send(Embed.SendError('Error', 'Please contact staff'))
-    }
-  }
+			console.log(participantRole)
 
-  return send(Embed.SendError('Failed', 'Invalid reference code'))
+			await guildMember.roles.add(participantRole)
+
+			await message.channel.send(
+				Embed.SendSuccess('Participant Role Granted', 'Enjoy Hackathon!'),
+			)
+		} catch (e) {
+			console.error(e)
+			message.channel.send(Embed.SendError('Error', 'Please contact staff'))
+		}
+	} else {
+		return message.channel.send(
+			Embed.SendError('Failed', 'Invalid reference code'),
+		)
+	}
 }
