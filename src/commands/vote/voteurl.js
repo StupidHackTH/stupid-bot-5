69d4b35ff1ce431925b9e059821338ad7e4f1a80
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2021-08-01 10:18:15 +0700

    added vote url

---
+++ src/commands/vote/voteurl.js
@@ -0,0 +1,46 @@
+const Embed = require('../../lib/Embed')
+const { guildId } = require('../../../config.json')
+
+module.exports = {
+	name: 'voteurl',
+	description: 'get voteurl',
+	hide: true,
+	async execute({ member, client }) {
+		const userId = member.id
+
+		const docRef = client.database.collection('Users').doc(userId)
+
+		try {
+			const userObj = (await docRef.get()).data()
+
+			var url = 'https://www.google.com/'
+
+			if (!userObj) {
+				/* if no user */
+				const sessionRef = await client.database.collection('VoteSession').add({
+					type: 'discord',
+					vote: {},
+				})
+				url += sessionRef.id
+				await docRef.set({ voteSessionId: sessionRef.id })
+			} else if (!userObj.voteSessionId) {
+				/* if have user but no session */
+				const sessionRef = await client.database.collection('VoteSession').add({
+					type: 'discord',
+					vote: {},
+				})
+				url += sessionRef.id
+				await docRef.set({ voteSessionId: sessionRef.id })
+			} else {
+				/* already have session */
+				url += userObj.voteSessionId
+			}
+
+			member.createDM().then((channel) => {
+				channel.send(url)
+			})
+		} catch (e) {
+			console.error(e)
+		}
+	},
+}
--- src/index.js
+++ src/index.js
@@ -11,41 +11,41 @@ client.cooldowns = new Discord.Collection()
 const http = require('http')
 
 http
-  .createServer((req, res) => {
-    res.write('helo world')
-    res.end()
-  })
-  .listen(process.env.PORT || 80)
+	.createServer((req, res) => {
+		res.write('helo world')
+		res.end()
+	})
+	.listen(process.env.PORT || 80)
 
 var firebaseConfig = {
-  apiKey: 'AIzaSyAXOlKvl4dCdfNPs7-ESbwLA7918tHgG1k',
-  authDomain: 'stupid-reference-code.firebaseapp.com',
-  projectId: 'stupid-reference-code',
-  storageBucket: 'stupid-reference-code.appspot.com',
-  messagingSenderId: '749205312711',
-  appId: '1:749205312711:web:300b7bdd185a002e1a9ae2',
-  measurementId: 'G-5NCC8DLM1L',
+	apiKey: 'AIzaSyAXOlKvl4dCdfNPs7-ESbwLA7918tHgG1k',
+	authDomain: 'stupid-reference-code.firebaseapp.com',
+	projectId: 'stupid-reference-code',
+	storageBucket: 'stupid-reference-code.appspot.com',
+	messagingSenderId: '749205312711',
+	appId: '1:749205312711:web:300b7bdd185a002e1a9ae2',
+	measurementId: 'G-5NCC8DLM1L',
 }
 
 // firebase setup
 firebase.initializeApp(firebaseConfig)
 firebase
-  .auth()
-  .signInWithEmailAndPassword(
-    ...process.env.AUDIENCE_APP_CREDENTIALS.split(':'),
-  )
-  .then(() => {
-    console.log(
-      'Authence app authenticated as',
-      firebase.auth().currentUser.uid,
-    )
-    client.database = firebase.firestore()
-    console.log('Link bot to database')
-  })
-  .catch((e) => {
-    console.log("Can't connect to database")
-    console.error(e)
-  })
+	.auth()
+	.signInWithEmailAndPassword(
+		...process.env.AUDIENCE_APP_CREDENTIALS.split(':')
+	)
+	.then(() => {
+		console.log(
+			'Authence app authenticated as',
+			firebase.auth().currentUser.uid
+		)
+		client.database = firebase.firestore()
+		console.log('Link bot to database')
+	})
+	.catch((e) => {
+		console.log("Can't connect to database")
+		console.error(e)
+	})
 
 loadCommands(client)
 loadEvents(client)
@@ -53,41 +53,41 @@ loadEvents(client)
 client.login(process.env.TOKEN)
 
 function loadCommands(client) {
-  const commandFolders = fs.readdirSync('./src/commands')
+	const commandFolders = fs.readdirSync('./src/commands')
 
-  for (const folder of commandFolders) {
-    const commandFiles = fs
-      .readdirSync(`./src/commands/${folder}`)
-      .filter((file) => file.endsWith('.js'))
-    for (const file of commandFiles) {
-      const command = require(`./commands/${folder}/${file}`)
-      client.commands.set(command.name, command)
+	for (const folder of commandFolders) {
+		const commandFiles = fs
+			.readdirSync(`./src/commands/${folder}`)
+			.filter((file) => file.endsWith('.js'))
+		for (const file of commandFiles) {
+			const command = require(`./commands/${folder}/${file}`)
+			client.commands.set(command.name, command)
 
-      console.log(`[Loaded command]: ${command.name}`)
-    }
-  }
+			console.log(`[Loaded command]: ${command.name}`)
+		}
+	}
 }
 
 function loadEvents(client) {
-  const eventFiles = fs
-    .readdirSync('./src/events')
-    .filter((file) => file.endsWith('.js'))
+	const eventFiles = fs
+		.readdirSync('./src/events')
+		.filter((file) => file.endsWith('.js'))
 
-  for (const file of eventFiles) {
-    const event = require(`./events/${file}`)
-    if (event.type === 'ws') {
-      client.ws.on(event.name, async (...args) => {
-        try {
-          await event.execute(...args, client)
-        } catch (e) {
-          console.error(e)
-        }
-      })
-    } else if (event.type === 'once') {
-      client.once(event.name, (...args) => event.execute(...args, client))
-    } else if (event.type === 'on') {
-      client.on(event.name, (...args) => event.execute(...args, client))
-    }
-    console.log(`[Loaded event]: ${event.name}`)
-  }
+	for (const file of eventFiles) {
+		const event = require(`./events/${file}`)
+		if (event.type === 'ws') {
+			client.ws.on(event.name, async (...args) => {
+				try {
+					await event.execute(...args, client)
+				} catch (e) {
+					console.error(e)
+				}
+			})
+		} else if (event.type === 'once') {
+			client.once(event.name, (...args) => event.execute(...args, client))
+		} else if (event.type === 'on') {
+			client.on(event.name, (...args) => event.execute(...args, client))
+		}
+		console.log(`[Loaded event]: ${event.name}`)
+	}
 }
